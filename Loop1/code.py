317b2fc37cae80f7c7d32f0fcd6ce8504b108600
del-luna-7 <7del.luna2008@gmail.com>
2025-02-16 18:48:55 +0400

    Loop1

---
+++ Loop1/code.py
@@ -0,0 +1,59 @@
+## Task 1
+# num1 = int(input("Введите начало диапазона: "))
+# num2 = int(input("Введите конец диапазона: "))
+#
+## 1
+# while num1 < num2:
+#     print(num1)
+#     num1 += 1
+#
+## 2
+# while num1 < num2:
+#     print(num2)
+#     num2 -= 1
+#
+## 3
+# while num1 <= num2:
+#     if num1 % 7 == 0:
+#         print(num1)
+#     num1 +=1
+#
+## 4
+# count = 0
+# while num1 <= num2:
+#     if num1 % 5 == 0:
+#         count += 1
+#     num1 += 1
+# print(f"Count: {count}")
+
+
+
+
+## Task 2
+# num1 = int(input("Введите начало диапазона: "))
+# num2 = int(input("Введите конец диапазона: "))
+#
+## 1
+# while num1 <= num2:
+#     if num1 % 3 == 0:
+#         print("Fizz")
+#     num1 += 1
+#
+## 2
+# while num1 <= num2:
+#     if num1 % 5 == 0:
+#         print("Buzz")
+#     num1 += 1
+#
+## 3
+# while num1 <= num2:
+#     if num1 % 3 == 0 and num1 % 5 == 0:
+#         print("Fizz Buzz")
+#     num1 += 1
+#
+## 4
+# while num1 <= num2:
+#     if num1 % 3 != 0 and num1 % 5 != 0:
+#         print(num1)
+#     num1 += 1
+
