82929f28040943865210ef3919bb70f2b692d81f
del-luna-7 <7del.luna2008@gmail.com>
2025-02-16 19:34:55 +0400

    FirstDz

---
+++ FirstDz/code.py
@@ -0,0 +1,59 @@
+# Task 1
+brand1 = "BMW"
+model1 = "M5"
+count1 = 10
+price1 = 63000
+speed1 = 305
+
+brand2 = "Hyundai"
+model2 = "Solaris"
+count2 = 7
+price2 = 40000
+speed2 = 190
+
+brand3 = "Toyota"
+model3 = "Supra MK4"
+count3 = 4
+price3 = 110000
+speed3 = 250
+
+carInfo1 = brand1, model1, count1, price1, speed1
+carInfo2 = brand2, model2, count2, price2, speed2
+carInfo3 = brand3, model3, count3, price3, speed3
+
+print(f'{carInfo1}, \n{carInfo2}, \n{carInfo3}')
+print('--------------------------------------------------')
+
+
+# Task 2
+num1 = 14
+num2 = 7
+
+summ = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f'{summ}, \n{subtraction}, \n{multiplication}, \n{division}')
+print('--------------------------------------------------')
+
+
+# Task 3
+num1 = 14
+num2 = 16
+num3 = 7
+
+sum_numbers = num1 + num2 + num3
+multiplication_numbers = num1 * num2 * num3
+
+print(f'Сумма чисел: {sum_numbers}, \nПроизведение чисел: {multiplication_numbers}')
+print('--------------------------------------------------')
+
+
+# Task 4
+salary = 1200
+bankCredit = 400
+utilities = 40
+
+print(f'Оставшиеся средства: {salary - (bankCredit + utilities)}')
+
